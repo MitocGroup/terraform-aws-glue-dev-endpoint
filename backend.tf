8b0cbc599c995bc53f503c1eb78061a74e78c8f3
Mihai Profir <mihai.profir@hivehome.com>
2019-07-18 10:53:12 +0100

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    region = "us-east-1"
-    bucket = "bucket_name"
-    key    = "path/to/terraform.tfstate"
-    workspace_key_prefix = "terraform_workspaces"
-  }
-}
