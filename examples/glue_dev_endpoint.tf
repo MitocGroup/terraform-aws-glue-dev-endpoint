1cfd11716a2764a3fea52b483c67143188174d45
Mihai Profir <mihai.profir.2@gmail.com>
2019-07-22 16:00:09 +0100

    DDO-771 upgrading and modifyiing stuff

---
+++ examples/glue_dev_endpoint.tf
@@ -0,0 +1,26 @@
+/* glue_database.tf
+ Glue database / table / crawler implementation
+module "dp_dl_prod_glue_projectx_00" {
+  source         = "../../" 
+*/
+
+/* main.tf
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  required_version = "~> 0.11.11"
+}  
+*/
+glue_endpoint_name	=
+glue_endpoint_role 
+glue_endpoint_security_group_ids
+glue_endpoint_subnet_id
+glue_endpoint_public_keys
+glue_endpoint_number_of_dpus
+glue_endpoint_extra_python_libraries
+glue_endpoint_extra_jars_libraries
+glue_endpoint_security_configuration
+/* 
+
