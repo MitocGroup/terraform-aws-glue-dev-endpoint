d0d6f9fafc638f570f22ce54793706b6562a858b
Mihai Profir <mihai.profir.2@gmail.com>
2019-07-23 14:19:56 +0100

    DDO-771 Adding tags DDO-771 https://jira.bgchtest.info/browse/DDO-771 https://jira.bgchtest.info/browse/DDO-771 https://jira.bgchtest.info/browse/DDO-771

--- examples/glue_dev_endpoint.tf
+++ examples/glue_dev_endpoint.tf
@@ -13,29 +13,33 @@ terraform {
   required_version = "~> 0.11.11"
 }  
 */
-/*
+
 module "glue-dev-endpoint" {
   source = "../"
-  glue_endpoint_name   = ""
-  glue_endpoint_role {
-  # Example IAM role
-  module "glue_role" {
-  source = "../"
-  name   = "dp-dl-test-role"
+  glue_endpoint_name                   = "test"
+  glue_endpoint_role                   = ""   #same role as the crawler
+  glue_endpoint_security_group_ids     = ""
+  glue_endpoint_subnet_id              = ""
+  glue_endpoint_public_keys            = ""
+  glue_endpoint_number_of_dpus         = ""
+  glue_endpoint_extra_python_libraries = ""
+  glue_endpoint_extra_jars_libraries   = ""
+  glue_endpoint_security_configuration = ""
 
+  tags = {
+    Type                 = "etl-test-job"
+    Origin               = "leak"
+    Source-Environment   = "dev"
+    Source-Data          = "events"
+    Update-Frequency     = "daily"
+    Documentation        = "https://confluence.bgchtest.info/display/DTS/Curated+data+sets"
+    Business-Owner       = "Robert Vadai"
+    Cost-Center          = "11111"
+    Classification-level = "2"
   }
-  glue_endpoint_security_group_ids =
-  glue_endpoint_subnet_id =
-  glue_endpoint_public_keys =
-  glue_endpoint_number_of_dpus =
-  glue_endpoint_extra_python_libraries =
-  glue_endpoint_extra_jars_libraries =
-  glue_endpoint_security_configuration =
-
-
 }
 
 
-*/
+
 
 
